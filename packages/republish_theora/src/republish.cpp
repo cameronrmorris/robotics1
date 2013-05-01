34f0e9296b26b9f93041de55d7a9df03022d32ca
Cameron Morris <zephyrz2712@gmail.com>
2013-05-01 16:50:05 -0400

    Maybe works

--- packages/republish_theora/src/republish.cpp
+++ packages/republish_theora/src/republish.cpp
@@ -2,13 +2,14 @@
 #include <sensor_msgs/CompressedImage.h>
 #include <sys/types.h>
 #include <signal.h>
+#include <time.h>
 
-int flag = 5;
+time_t last_seen;
 
 void messageCallBack( const sensor_msgs::CompressedImage::ConstPtr& msg ) {
 
   ROS_INFO( "Message detected\n");
-  flag = 5;
+  time(&last_seen);
 
 }
 
@@ -41,27 +42,31 @@ int main( int argc, char *argv[] ) {
 
   default: // Monitor
 
+    time( &last_seen );
+    sleep( 1 );
+
     ROS_INFO( "Created republisher node with pid : %d", pid );
     ros::init(argc, argv, "republish_monitor" );
-
+    time_t current;
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe( argv[2], 10, messageCallBack );
     
     ros::Rate loop_rate(10);
 
-    //   sleep(5000);
-
     while( ros::ok() ) {
 
       ros::spinOnce();
-      flag--;
+      
+      time(&current);
+      
+      ROS_INFO( "Last seen: %f seconds ago", difftime( current, last_seen ) );
 
       // Probably not working, KILL THYSELF
-      if( flag < 0 ) {
+      if( difftime( current, last_seen) > 5 ) {
 	
 	ROS_INFO( "KILLING REPUBLISHER\n");
 	// Kill child
-	//	kill(pid, SIGTERM);
+	kill(pid, SIGTERM);
 	
 	wait();
 	exit(-1);
